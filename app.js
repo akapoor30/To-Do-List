9fc5ea523205e8a61d7c145034159e17db2c546b
ratedHunter <anshkapoor502@gmail.com>
2023-02-07 02:47:28 +0530

    intial

---
+++ app.js
@@ -0,0 +1,37 @@
+let input = document.querySelector('.entered-list');
+let addBtn = document.querySelector('.add-list');
+let tasks = document.querySelector('.tasks');
+
+
+input.addEventListener('keyup', () => {
+    if (input.value.trim() != 0) {
+        addBtn.classList.add('active')
+    } else {
+        addBtn.classList.remove('active')
+    }
+})
+
+addBtn.addEventListener('click', () => {
+    // console.log("qwery")
+    if (input.value.trim() != 0) {
+        let newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+        <p> ${input.value}</p>
+        <div class="item-btn">
+            <i class="fa-solid fa-pen-to-square"></i>
+            <i class="fa-solid fa-xmark"></i>
+        </div>`
+
+        tasks.appendChild(newItem);
+        input.value = '';
+    } else {
+        alert('Please enter a task')
+    }
+})
+
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-xmark')) {
+        e.target.parentElement.parentElement.remove();
+    }
+})
